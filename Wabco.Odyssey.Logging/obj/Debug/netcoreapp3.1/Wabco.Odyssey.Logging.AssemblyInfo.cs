3333d17ef489a8849b41361972c0ccba64a55fbd
katyayanidotnet <katyayani.dotnet@gmail.com>
2020-06-16 13:28:07 +0530

    added files

--- Wabco.Odyssey.Logging/obj/Debug/netcoreapp3.1/Wabco.Odyssey.Logging.AssemblyInfo.cs
+++ Wabco.Odyssey.Logging/obj/Debug/netcoreapp3.1/Wabco.Odyssey.Logging.AssemblyInfo.cs
@@ -11,8 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Wabco.Odyssey.Logging")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Cybage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Serilog library to log application events")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wabco.Odyssey.Logging")]
